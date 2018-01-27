4915d5efe5bff1e18efeb792dda54f6297861538
Saran Tanpituckpong <sarunta@gmail.com>
2018-01-27 17:33:51 +0700

    :lipstick: Update navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
 #app
-	nav.navbar(role='navigation')
+	nav.navbar.is-light(role='navigation')
 		.container
 			.navbar-brand
-				a.navbar-item.has-text-weight-bold.has-text-primary(
+				a.navbar-item.has-text-weight-bold(
 					href='./'
 				) #[b-icon#brand-icon(icon='heart', size='is-medium')]ESLint Config Gluons
-			.navbar-menu
+				div.navbar-burger(@click='burgerClick')
+					span(v-for='i in 3')
+			.navbar-menu.is-light(ref='navmenu')
 				.navbar-start
 					a.navbar-item(href='./') Home
 	home
@@ -29,6 +31,20 @@ export default {
 	name: 'App',
 	components: {
 		Home
+	},
+	methods: {
+		burgerClick(e) {
+			let burger = e.target;
+			let navmenu = this.$refs.navmenu;
+
+			burger.classList.toggle('is-active');
+			// Safe toggle
+			if (burger.classList.contains('is-active')) {
+				navmenu.classList.add('is-active');
+			} else {
+				navmenu.classList.remove('is-active');
+			}
+		}
 	}
 };
 </script>
