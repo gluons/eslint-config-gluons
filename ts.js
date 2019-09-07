71e01284346ea84f9b091c91b96a8104748c2215
Saran Tanpituckpong <sarunta@gmail.com>
2019-09-07 21:17:18 +0700

    :wrench: Update rule

--- ts.js
+++ ts.js
@@ -19,6 +19,7 @@ module.exports = {
 	plugins: ['@typescript-eslint'],
 	rules: {
 		'@typescript-eslint/brace-style': ['error', '1tbs'],
+		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/indent': [
 			'error',
 			'tab',
