1900e20730333d60d95b72abe8d316cd8c06f6b0
Saran Tanpituckpong <sarunta@gmail.com>
2025-10-18 04:33:51 +0700

    🧹 Remove deprecated stylistic rule configurations and update member ordering in TypeScript rules

--- src/rules/ts.ts
+++ src/rules/ts.ts
@@ -14,62 +14,31 @@ const config: Linter.Config = {
 		}
 	},
 	rules: {
-		'brace-style': 'off',
-		// '@typescript-eslint/brace-style': ['error', '1tbs'], // This rule doesn't exist in @typescript-eslint
 		'@typescript-eslint/explicit-function-return-type': 'off',
-		'comma-spacing': 'off',
-		// '@typescript-eslint/comma-spacing': [ // This rule doesn't exist in @typescript-eslint
-		//   'error',
-		//   {
-		//     before: false,
-		//     after: true
-		//   }
-		// ],
-		indent: 'off',
-		// '@typescript-eslint/indent': [ // This rule doesn't exist in @typescript-eslint
-		//   'error',
-		//   2,
-		//   {
-		//     SwitchCase: 1
-		//   }
-		// ],
-		'keyword-spacing': 'off',
-		// '@typescript-eslint/keyword-spacing': [ // This rule doesn't exist in @typescript-eslint
-		//   'error',
-		//   {
-		//     before: true,
-		//     after: true
-		//   }
-		// ],
-		quotes: 'off',
-		// '@typescript-eslint/quotes': [ // This rule doesn't exist in @typescript-eslint
-		//   'error',
-		//   'single',
-		//   {
-		//     avoidEscape: true,
-		//     allowTemplateLiterals: true
-		//   }
-		// ],
 		'@typescript-eslint/member-ordering': [
 			'warn',
 			{
 				default: [
+					'#private-static-field',
 					'private-static-field',
 					'protected-static-field',
 					'public-static-field',
 
+					'#private-static-method',
 					'private-static-method',
 					'protected-static-method',
 					'public-static-method',
-
-					'private-instance-field',
-					'protected-instance-field',
-					'public-instance-field',
 
 					'private-constructor',
 					'protected-constructor',
 					'public-constructor',
 
+					'#private-instance-field',
+					'private-instance-field',
+					'protected-instance-field',
+					'public-instance-field',
+
+					'#private-instance-method',
 					'private-instance-method',
 					'protected-instance-method',
 					'public-instance-method'
@@ -86,17 +55,6 @@ const config: Linter.Config = {
 				argsIgnorePattern: '^_'
 			}
 		],
-		'space-before-function-paren': 'off',
-		// '@typescript-eslint/space-before-function-paren': [ // This rule doesn't exist in @typescript-eslint
-		//   'error',
-		//   {
-		//     anonymous: 'always',
-		//     named: 'never',
-		//     asyncArrow: 'always'
-		//   }
-		// ],
-		semi: 'off',
-		// '@typescript-eslint/semi': ['error', 'always'], // This rule doesn't exist in @typescript-eslint
 		'@typescript-eslint/triple-slash-reference': 'off'
 	}
 };
