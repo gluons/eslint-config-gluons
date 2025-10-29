f019093f432b027513ce5495a527407bdf6c6c39
Saran Tanpituckpong <sarunta@gmail.com>
2025-10-29 15:30:06 +0700

    🔥 Remove `mixed.ts` configuration file

--- src/mixed.ts
+++
@@ -1,7 +0,0 @@
-import type { Linter } from 'eslint';
-import esnextConfig from './esnext.js';
-import tsConfig from './ts.js';
-
-const config: Linter.Config[] = [...esnextConfig, ...tsConfig];
-
-export default config;
