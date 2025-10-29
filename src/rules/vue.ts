ddac42c4c554d12f301e9d7fd012f605499e1ee3
Saran Tanpituckpong <sarunta@gmail.com>
2025-10-29 17:56:46 +0700

    ♻️ Refactor `vue.ts` to use `defineConfig` for ESLint
    Updated the configuration to utilize the new flat config format
    from `eslint-plugin-vue`, improving maintainability and clarity.

--- src/rules/vue.ts
+++ src/rules/vue.ts
@@ -1,33 +1,33 @@
-import type { Linter } from 'eslint';
+import { defineConfig } from 'eslint/config';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
 
-const config: Linter.Config = {
-	files: ['**/*.vue'],
-	plugins: {
-		vue: require('eslint-plugin-vue')
-	},
-	languageOptions: {
-		parser: require('vue-eslint-parser'),
-		parserOptions: {
-			ecmaVersion: 2022,
-			sourceType: 'module'
+const configs = defineConfig([
+	pluginVue.configs['flat/recommended'],
+	{
+		languageOptions: {
+			sourceType: 'module',
+			globals: {
+				...globals.browser
+			}
+		},
+		rules: {
+			'vue/html-indent': ['error', 'tab'],
+			'vue/script-indent': [
+				'error',
+				'tab',
+				{
+					switchCase: 1
+				}
+			],
+			'vue/html-closing-bracket-newline': [
+				'error',
+				{
+					multiline: 'always'
+				}
+			]
 		}
-	},
-	rules: {
-		'vue/html-indent': ['error', 'tab'],
-		'vue/script-indent': [
-			'error',
-			'tab',
-			{
-				switchCase: 1
-			}
-		],
-		'vue/html-closing-bracket-newline': [
-			'error',
-			{
-				multiline: 'always'
-			}
-		]
 	}
-};
+]);
 
-export default config;
+export default configs;
