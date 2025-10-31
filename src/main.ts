093463bbd6beda0496ff35272fbb4cf1707eb928
Saran Tanpituckpong <sarunta@gmail.com>
2025-11-01 03:22:09 +0700

    🎨 Refactor formatting in src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import mainRules from './rules/main.js';
 
-const config = defineConfig([
-	mainRules,
-	eslintPluginPrettierRecommended
-]);
+const config = defineConfig([mainRules, eslintPluginPrettierRecommended]);
 
 export default config;
