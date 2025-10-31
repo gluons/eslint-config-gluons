2273f3ed1c76528b7b3bccabc5826503069c31e9
Saran Tanpituckpong <sarunta@gmail.com>
2025-10-31 11:26:20 +0700

    🔧 Add Jest configuration and `tsconfig.test.json`
    Introduce a new Jest configuration in `jest.config.ts` to enable
    TypeScript testing with `ts-jest`. Add `tsconfig.test.json` for
    custom TypeScript compiler options during testing.

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,15 @@ import type { Config } from 'jest';
 import { createDefaultEsmPreset } from 'ts-jest';
 
 const config: Config = {
-	...createDefaultEsmPreset()
+	...createDefaultEsmPreset(),
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.test.json'
+			}
+		]
+	}
 };
 
 export default config;
