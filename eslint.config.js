393f662867a3df54881982c89d0777c4561f9a9a
Saran Tanpituckpong <sarunta@gmail.com>
2026-01-14 11:35:36 +0700

    🔧 Update `eslint.config.js` to just `recommended` configs
    Drop XO.

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,25 @@
-import { defineConfig } from 'eslint/config';
-import xoTypeScript from 'eslint-config-xo-typescript';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
-/*
- * Fix `Key "languageOptions": allowTrailingCommas option is only available in JSONC.` error
- * See https://github.com/xojs/xo/issues/798#issuecomment-3044681378
- */
-const xoTypeScriptWithoutJSON = xoTypeScript.filter(
-	config => !config.language?.startsWith('json/')
-);
-
-/*
- * Fix `Key "plugins": Cannot redefine plugin "@stylistic"` error
- * Remove the '@stylistic' plugin from `eslint-config-xo-typescript` because it is already included in `eslint-config-xo`
- */
-xoTypeScriptWithoutJSON.forEach(config => {
-	if (
-		config.plugins?.['@typescript-eslint'] &&
-		config.plugins['@stylistic']
-	) {
-		config.languageOptions.parserOptions = {
-			...config.languageOptions.parserOptions,
-			project: './tsconfig.eslint.json',
-			tsconfigRootDir: import.meta.dirname
-		};
-		config.ignores = [...(config.ignores ?? []), 'eslint.config.js'];
-
-		delete config.languageOptions.parserOptions.projectService;
-		delete config.plugins['@stylistic'];
-	}
-});
-
-export default defineConfig([
-	xoTypeScriptWithoutJSON,
+export default [
 	{
-		rules: {
-			'@typescript-eslint/naming-convention': 'off'
-		}
+		ignores: ['dist/**', 'test/fixtures/**']
 	},
-	eslintPluginPrettierRecommended,
-	{
-		ignores: ['dist/', 'test/fixtures/']
-	}
-]);
+	js.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked.map(config => ({
+		...config,
+		files: ['**/*.ts'],
+		languageOptions: {
+			...config.languageOptions,
+			parserOptions: {
+				...config.languageOptions?.parserOptions,
+				project: ['./tsconfig.eslint.json'],
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+	})),
+	stylistic.configs.recommended,
+	prettierRecommended
+];
