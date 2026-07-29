c9086a2e5fa1ffae9dd2769c315d127d863b33ca
Saran Tanpituckpong <sarunta@gmail.com>
2026-07-30 02:40:49 +0700

    ♻️ Extract shared rule page styles into `rules-shared.css`

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
+import './styles/rules-shared.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'buefy/dist/css/buefy.css';
 import App from './App.vue';
